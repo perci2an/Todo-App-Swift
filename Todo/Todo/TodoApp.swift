bb3bbfc1994717a904a43ef0e5de495dd15e5d23
perci2an <psmim1216@naver.com>
2024-08-01 14:27:29 +0900

    Todo 생성

---
+++ Todo/Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 박승민 on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
